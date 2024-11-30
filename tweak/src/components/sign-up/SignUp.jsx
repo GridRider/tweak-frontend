82ec5416cae511a361f187696ef98355a24bbf05
GridRider <anoopthengin@gmail.com>
2024-11-30 22:26:06 +0530

    changes related to issue 3 and 4 has also been added.

---
+++ tweak/src/components/sign-up/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+      <button>go back</button>
+      <h1>Welcome to Tweak</h1>
+      <form action=""></form>
+    </div>
+  )
+}
+
+export default SignUp
