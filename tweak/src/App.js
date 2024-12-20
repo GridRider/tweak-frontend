e859f225c46808463052700b123edfcceb80fd4a
GridRider <anoopthengin@gmail.com>
2024-12-20 19:14:38 +0530

    Description page for each tweak has been added.

--- tweak/src/App.js
+++ tweak/src/App.js
@@ -7,6 +7,7 @@ import SignUp from './components/sign-up/SignUp';
 import CreateTweak from './components/create-tweak/CreateTweak';
 import ListTweak from './components/list-tweak/ListTweak';
 import EachTweak from './components/list-tweak/EachTweak';
+import DescriptionTweak from './components/description-tweak/DesriptionTweak';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/create-tweak" element={<CreateTweak/>}/>
         <Route path="/list-tweak" element={<ListTweak/>}/>
         <Route path="/each-tweak" element={<EachTweak/>}/>
+        <Route path="/description-tweak" element ={<DescriptionTweak/>}/>
       </Routes>
     </div>
   );
