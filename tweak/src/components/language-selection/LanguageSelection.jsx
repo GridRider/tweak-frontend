c81aa36813007a023f79490dbca10d7058afe0c7
GridRider <anoopthengin@gmail.com>
2024-12-25 01:32:52 +0530

    Changes to UI of language selection page

--- tweak/src/components/language-selection/LanguageSelection.jsx
+++ tweak/src/components/language-selection/LanguageSelection.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import styles from "./LanguageSelection.module.css"
+import { useNavigate } from 'react-router-dom'
 
 function LanguageSelection() {
+  const navigate=useNavigate();
+  const onLanClick=()=>{
+    navigate("/");
+  }
   return (
-    <div>
-      <p>Choose your language</p>
-      <button>English</button>
-      <button>Malayalam</button>
-      <button>Tamil</button>
-      <button>Hindi</button>
-    </div>
+    <>
+      <p className={styles.lang}>Choose your language</p>
+      <div className={styles["lang-btn-container"]}>
+      <button onClick={onLanClick}>English</button>
+      <button onClick={onLanClick}>Malayalam</button>
+      <button onClick={onLanClick}>Tamil</button>
+      <button onClick={onLanClick}>Hindi</button>
+      </div>
+    </>
   )
 }
 
